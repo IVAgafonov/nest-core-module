e19d4c53da3a3f9fa18c5a3072da5b8c4b74189b
IAgafonov <igoradm90@yandex.ru>
2021-02-01 17:38:32 +0300

    fix module scope

--- src/nest-core.module.ts
+++ src/nest-core.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { PrometheusService } from './services/prometheus/prometheus.service';
 import { PrometheusController } from './api/controllers/prometheus.controller';
 import { LoggerServiceFactory } from './services/logger/logger.service';
 import { TestController } from './api/controllers/test.controller';
 
+@Global()
 @Module({
   imports: [],
   controllers: [PrometheusController, TestController],
