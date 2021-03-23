75e4b27ecdff24226986effa5e922230ab381065
IAgafonov <igoradm90@yandex.ru>
2021-03-23 21:00:54 +0300

    add entry point

---
+++ src/index.d.ts
@@ -0,0 +1,4 @@
+export * from './nest-core.module';
+export * from './services/prometheus/prometheus.service';
+export * from './services/logger/logger.service';
+export * from './api/controllers/prometheus.controller';
